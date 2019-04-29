ebed989131d6557a7f07ea7d333f2815bc01e936
Abby Sova <43381584+abbysova@users.noreply.github.com>
2019-04-29 17:50:20 -0500

    Working on GUI (April-29)

---
+++ app/src/main/java/com/example/final_mobile_device_programming/map.java
@@ -0,0 +1,13 @@
+package com.example.final_mobile_device_programming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
